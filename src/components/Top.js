8f0038317b7962cbbde64fb3026eea0509ca1314
aion0721 <kobayashi@test.com>
2019-10-22 20:50:44 +0900

    create header

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,11 +1,51 @@
-import React from 'react'
 
-function Top(){
-    return(
-        <div>
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function Top() {
+  const classes = useStyles();
+
+  return (
+      <React.Fragment>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
             Top
-        </div>
-    )
+          </Typography>
+          <Button color="inherit">LogOut</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+    <div>
+        <p> Contents. </p>
+        <p> Contents. </p>
+        <p> Contents. </p>
+        <p> Contents. </p>
+        <p> Contents. </p>
+    </div>
+    </React.Fragment>
+  );
 }
-
 export default Top
