be92d1160b77c301d0e116b18b290ff8bb7607d6
aion0721 <kobayashi@test.com>
2019-10-22 20:34:38 +0900

    create link to top page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,37 +1,33 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-import { makeStyles } from '@material-ui/core/styles';
+import { withRouter } from 'react-router';
 
-const useStyles = makeStyles(theme => ({
-    container: {
-        display:'flex',
-        flexWrap: 'wrap',
-        align: 'center'
-    },
-    textField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(1),
-        width:200,
+
+class Login extends React.Component {
+
+    handleToTopPage = () => {
+        this.props.history.push('/top')
     }
-}))
 
-function Login (){
-    const classes = useStyles();
-
-    return (
-        <form className={classes.container} noValidate autoComplete="off">
+    render() {
+        return(
+        <form noValidate autoComplete="off">
             <div>
                 Name:<TextField 
-                    className={classes.textField}
                 /><br />
                 Password:<TextField 
                     type="password"
                 /><br />
-                <Button variant="contained" color='primary'>LOGIN!</Button>
+                <Button
+                    variant="contained"
+                    color='primary'
+                    onClick={this.handleToTopPage}>
+                LOGIN!</Button>
             </div>
         </form>
-    )
+        )
+    }
 }
 
-export default Login
+export default withRouter(Login)
