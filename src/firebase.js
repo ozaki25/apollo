501d96f0ddd4fe2a89f80e1ab3fe5b9f02c9409c
aion0721 <kobayashi@test.com>
2019-10-28 22:43:50 +0900

    firebase auth

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+
+  // Your web app's Firebase configuration
+  const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY , 
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN ,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL ,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID ,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET ,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID ,
+    appId: process.env.REACT_APP_FIREBASE_APPID ,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID ,
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(config)
+
+export default firebase
